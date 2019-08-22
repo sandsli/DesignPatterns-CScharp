677c5aba822c6e5c6864ef2c8afb8628162a0120
sandsli <543793753@qq.com>
2019-08-23 00:52:50 +0800

    a

---
+++ DesignPatterns/BuilderPattern/Builder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BuilderPattern
+{
+    public abstract class Builder
+    {
+        public abstract void BuildPart();
+    }
+
+    public class ConcreteBuilder : Builder
+    {
+        public override void BuildPart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 指挥者
+    /// </summary>
+    public class Director
+    {
+
+    }
+
+    /// <summary>
+    /// 产品
+    /// </summary>
+    public abstract class Product
+    {
+
+    }
+}
